77c0e2bf6f3993b71bbdfd4a5c3b9f912c6ffa6c
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-04-27 11:30:54 +0700

    Update scraper.go

--- src/be/pkg/scraper/scraper.go
+++ src/be/pkg/scraper/scraper.go
@@ -8,10 +8,58 @@ import (
 	"golang.org/x/net/html"
 )
 
+<<<<<<< Updated upstream
 // ExtractLinks mengambil semua link dari halaman web Wikipedia
 func ExtractLinks(url string) ([]string, error) {
 	// Mendapatkan isi halaman web
 	resp, err := http.Get(url)
+=======
+var (
+	ArticleCount int = 0
+)
+
+func QueueColly(input, output *queue.Queue, start, ends string, con *config.Config, parents *sync.Map) *set.MapString {
+	var results = set.NewSetOfSlice()
+
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.Async(con.IsAsync),
+		colly.MaxDepth(con.MaxDepth),
+		colly.UserAgent("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_4_1) AppleWebKit/536.25 (KHTML, like Gecko) Chrome/51.0.2823.231 Safari/537"),
+		colly.CacheDir(con.CacheDir),
+		colly.AllowedDomains(con.AllowedDomains...),
+	)
+
+	extensions.RandomUserAgent(c)
+
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second, // Timeout
+			KeepAlive: 30 * time.Second, // keepAlive timeout
+		}).DialContext,
+		MaxIdleConns:          100,              // Maximum number of idle connections
+		IdleConnTimeout:       90 * time.Second, // Idle connection timeout
+		TLSHandshakeTimeout:   10 * time.Second, // TLS handshake timeout
+		ExpectContinueTimeout: 1 * time.Second,
+	})
+
+	if p, err := proxy.RoundRobinProxySwitcher(
+		"http://103.59.45.53:8080",
+		"http://36.64.217.27:1313",
+		"http://101.255.166.242:8080",
+		"http://8c5f3b01ca4c7c3cd1c7ee83e4b652f69d6bd21b:@proxy.zenrows.com:8001",
+	); err == nil {
+		c.SetProxyFunc(p)
+	}
+
+	err := c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: con.MaxParallelism,
+		RandomDelay: con.RandomDelay,
+	})
+
+>>>>>>> Stashed changes
 	if err != nil {
 		return nil, fmt.Errorf("gagal melakukan GET request: %v", err)
 	}
